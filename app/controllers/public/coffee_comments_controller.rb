820f22f9cbbb013c3156ccf864a07d672e0f8638
zero-0017 <hiromichi.v.0017@gmail.com>
2022-10-04 05:31:27 +0000

    [会員側]コメント機能を非同期通信化

--- app/controllers/public/coffee_comments_controller.rb
+++ app/controllers/public/coffee_comments_controller.rb
@@ -1,16 +1,15 @@
 class Public::CoffeeCommentsController < ApplicationController
 
   def create
-    post_coffee = PostCoffee.find(params[:post_coffee_id])
+    @post_coffee = PostCoffee.find(params[:post_coffee_id])
     comment = current_user.coffee_comments.new(coffee_comment_params)
-    comment.post_coffee_id = post_coffee.id
+    comment.post_coffee_id = @post_coffee.id
     comment.save
-    redirect_to request.referer
   end
 
   def destroy
+    @post_coffee = PostCoffee.find(params[:post_coffee_id])
     CoffeeComment.find(params[:id]).destroy
-    redirect_to request.referer
   end
 
   private
