efb3960305075c0ba1c7f464bffc20ac52aa25cb
zero-0017 <hiromichi.v.0017@gmail.com>
2022-10-08 09:12:27 +0000

    [会員側]about&サイドバーのレイアウト調整,footer追加,全体の文字の書体変更

--- app/controllers/public/searchs_controller.rb
+++ app/controllers/public/searchs_controller.rb
@@ -2,5 +2,7 @@ class Public::SearchsController < ApplicationController
   def search
     @post_coffees = PostCoffee.looks(params[:search], params[:word])
     @tags = Tag.all
+    @categorys = Category.all
+    @genres_list = Genre.all
   end
 end
