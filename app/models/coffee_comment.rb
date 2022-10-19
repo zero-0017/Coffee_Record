82a79e599d3348caeb4cccb5624e4464e2871c95
zero-0017 <hiromichi.v.0017@gmail.com>
2022-10-19 01:49:57 +0000

    [会員側]コメント削除不可だった為、修正

--- app/models/coffee_comment.rb
+++ app/models/coffee_comment.rb
@@ -1,7 +1,6 @@
 class CoffeeComment < ApplicationRecord
   belongs_to :user
   belongs_to :post_coffee
-  has_many :notifications, dependent: :destroy
 
 validates :comment, presence: true, length: {maximum: 100}
 end
